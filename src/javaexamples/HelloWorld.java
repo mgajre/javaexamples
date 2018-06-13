0cbf3d332cd529dce37698c5f3ef1040113690c5
manjeetgajre <manjeetgajre@192.168.0.13>
2018-06-12 18:48:50 -0700

    Hello World Program

---
+++ src/javaexamples/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Creating my own package for all java examples
+ */
+package javaexamples;
+
+/*
+ * Defining a public class HelloWorld
+ */
+public class HelloWorld {
+	
+	/*
+	 * the mail function call, the entry point to the program
+	 */
+	public static void main(String args[]) {
+		
+		/*
+		 * printing Hello World to the console
+		 */
+		System.out.println("Hello World!");
+		
+	}
+}
